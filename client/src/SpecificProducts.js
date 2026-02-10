6b05469631a9b7a53e11f0249d5df3a8fbce8586
karthikeyan4019 <131947262+karthikeyan4019@users.noreply.github.com>
2026-02-10 21:07:46 +0530

    Update SpecificProducts.js

--- client/src/SpecificProducts.js
+++ client/src/SpecificProducts.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 //redux
-import {useSelector } from "react-redux";
+// import {useSelector } from "react-redux";
 // css file
 import"./ProductsShow.css";
 
